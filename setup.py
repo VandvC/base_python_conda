e4d3f4fb214e89d250bbe525c56c0745b1b5c830
Constantin VdV <vandevelde-c@hotmail.fr>
2021-02-08 09:47:29 +0100

    [ADD] Original repo

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    url='https://github.com/VandvC/paris_estate_price',
+    author='https://github.com/VandvC',
+    description='Original code base ',
+    packages=find_packages())
